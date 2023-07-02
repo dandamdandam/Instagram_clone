4d1e4b05e649bd461d09b52bb253dd088f55d4d2
dandamdandam <glue0440@gmail.com>
2023-07-02 11:23:36 +0900

    Init

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import Post from './components/post_component';
+import Story from './components/story_component';
+
+function App() {
+  return (
+    <div className="App">
+      <StoryArr/>
+      <PostArr />
+      <Navbar />
+    </div>
+  );
+}
+
+/**
+ * 스토리 묶음
+ */
+function StoryArr() {
+
+}
+
+/**
+ * 게시물 묶음
+ */
+function PostArr() {
+
+}
+
+function Navbar() {
+
+}
+
+export default App;
---
+++ client/src/components/story_component.js
@@ -0,0 +1,5 @@
+function Story() {
+
+}
+
+export default Story;
