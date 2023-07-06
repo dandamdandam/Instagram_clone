31bfb77d96054fa4c1ebda61bbeac27076234502
dandamdandam <glue0440@gmail.com>
2023-07-06 19:52:59 +0900

    feat story
    스토리 컴포넌트 완성

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import Post from './components/post_component';
 import Story from './components/story_component';
+import get_random_profiles from './javascripts/get_random_profiles';
+import {useState, useEffect} from 'react';
+import './style.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <StoryArr/>
       <PostArr />
       <Navbar />
@@ -11,11 +15,53 @@ function App() {
   );
 }
 
+function Header(){
+  return(
+    <div className="header">
+      <img src="assets/images/logo.jpg"/>
+      <img src="assets/images/like_off_icon.jpg"/>
+      <img src="assets/images/direct_msg_icon.jpg"/>
+    </div> 
+  )
+}
+
 /**
  * 스토리 묶음
  */
 function StoryArr() {
+  var [profiles]=useState(get_random_profiles(30));
+  var [new_story_num]=useState(Math.random() * profiles.length + 1);
 
+  /* 스토리 컨테이너 이동 */
+  var [con_margin, con_move]=useState(0);
+  function move_left(){
+    if(con_margin!=0)
+      con_move(con_margin+document.body.clientWidth);
+  };
+  function move_right(){
+    con_move(con_margin-document.body.clientWidth);
+  }
+  var move = {
+    transition: 'all ease-in-out 0.5s',
+    transform: 'translate('+con_margin+'px)'
+  }
+
+  return(
+    <div style = {{height: '111.333px'}}>
+      <div className="story_container" style={move}>
+        <Story img_path={"assets/images/default_image.jpg"} id={"내 스토리"} is_new={true}/>
+        {
+          profiles.map(function(i){
+            return <Story img_path={i[1]} id={i[2]} is_new={i[0]<new_story_num ? true:false}/>
+          })
+        }
+      </div>
+      <div className="story_arrow">
+        <img src="assets/images/ei_arrow-left.png" onClick={move_left}/>
+        <img src="assets/images/ei_arrow-right.png" onClick={move_right}/>
+      </div>
+    </div>
+  );
 }
 
 /**
@@ -26,7 +72,15 @@ function PostArr() {
 }
 
 function Navbar() {
-
+  return(
+    <div className="nav_bar">
+      <img src="assets/images/bottom_nav_home_on_icon.jpg"/>
+      <img src="assets/images/bottom_nav_search_off_icon.jpg"/>
+      <img src="assets/images/bottom_nav_upload_icon.jpg"/>
+      <img src="assets/images/bottom_nav_reels_off_icon.jpg"/>
+      <img src="assets/images/default_image.jpg"/>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/story_component.js
+++ client/src/components/story_component.js
@@ -1,5 +1,21 @@
-function Story() {
+function Story({img_path, id, is_new}) {
+    var colored_ring={
+        backgroundImage: 'url("assets/images/story_ring_colored.png")',
+        textAlign: 'center'
+    };
+    var noncolored_ring={
+        backgroundImage: 'url("assets/images/story_ring_noncolored.png")',
+        textAlign: 'center'
+    }
 
+    return (
+        <div className="story">
+            <div className="ring" style={ is_new ? colored_ring : noncolored_ring}>
+                <img src={img_path}/>
+            </div>
+            <span className="id">{id}</span>
+        </div>
+    );
 }
 
 export default Story;
