1cce19891007b48b169572426c2b577fcd56b5a6
jjh4450 <jjh4450jjh4450@gmail.com>
2023-07-11 01:26:35 +0900

    Ready to read json

--- client/src/sugesstions/Suggesstions.js
+++ client/src/sugesstions/Suggesstions.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './Suggesstions.css'
 import Post from './post/Post'
 
+
 function PostArr() {
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+    fetch("/jsons/Suggesstions.json")
+    .then(res => res.json())
+    .then(data => setPost(data));
+  }, []);
+
+  useEffect(() => {
+    console.log(post);
+  }, [post]);
+
   return (
     <div>
       <Post />
+      {post.photo}
       <Post />
       <Post />
     </div>
