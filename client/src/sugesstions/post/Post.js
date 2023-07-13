d74fde00b6eebc0a3fbbfe2a0fed1b2496f42769
jjh4450 <jjh4450jjh4450@gmail.com>
2023-07-14 03:28:13 +0900

    Feat/chojunhwan (#4)
    * Ready to read json
    
    * Make Post tags generated by json

--- client/src/sugesstions/post/Post.js
+++ client/src/sugesstions/post/Post.js
@@ -5,20 +5,18 @@ import { Avatar } from '@mui/material'
 import { MoreHoriz, FavoriteBorder, ChatBubbleOutlineRounded, Telegram, BookmarkAddTwoTone } from '@mui/icons-material'
 // import { get_random_profiles } from '../../javascripts/get_random_profiles.js'
 
-function Post() {
+function Post(props) {
   return ( 
     <div className='post'>
       <div className='post__header'>
         <div className='post__headerLeft'>
-          <Avatar className='post__avatar' alt='username' src='assets/random_pfp/pfp3.jpg' />
-          foo • <span>a year ago</span>
-          {/*https://open.spotify.com/track/6fIoibjLOuU8I3NJbF1UOi?si=8c6d7b883a294932*/}
+          <Avatar className='post__avatar' alt={props.authorName} src={props.authorImage} />
+          {props.authorName} • <span>{props.timestamp}</span>
         </div>
         <MoreHoriz className='post__headerRightButton' />
       </div>
       <div className='post__body'>
-        {/* to-do 이미지 링크 받기*/}
-        <img src='https://source.unsplash.com/category/insta' alt='' />
+        <img src={props.postImage} alt='' />
       </div>
       <div className='post__footer'>
         <div className='post__footerLeft'>
@@ -27,16 +25,15 @@ function Post() {
           <Telegram className='postIcon' />
         </div>
         <div className='post__footerCenter'>
-          {/* to-do 이미지 수에 따라 동적으로 렌더링*/}
+          {/* to-do 이미지 수에 따라 동적으로 렌더링 •••••• 넘기는 기능 */}
         </div>
         <div className='post__footerRight'>
           <BookmarkAddTwoTone className='postIcon' />
         </div>
       </div>
       <div className='post__comment'>
-        {/* to-do 댓글 렌더링*/}
-        28 likes<br/>
-        comments...
+        {props.likes} likes<br/>
+        {props.comment}
       </div>
     </div>
   )
